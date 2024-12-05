83bc115eacdc2f6d051f3c1f155e2eb3ab8552b4
Muhammad Ragil <mragiltrirezar@gmail.com>
2024-12-05 23:09:46 +0700

    feat: root route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,5 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-	import { Button } from '$lib/components/ui/button';
-	import { Input } from '$lib/components/ui/input';
-
-	let cashier = $state('');
-	let buttonDisabled = $derived(!cashier);
-	$inspect(buttonDisabled);
-
-	const navigateToCashier = (e: SubmitEvent) => {
-		e.preventDefault();
-		goto(`/pos?cashier=${cashier}`);
-	};
-</script>
-
-<form class="mx-auto p-6 text-center" onsubmit={navigateToCashier}>
-	<Input class="mb-10" bind:value={cashier} />
-	<Button disabled={buttonDisabled} type={"submit"}>Enter as Cashier</Button>
-</form>
+<div class="h-screen flex items-center justify-center">
+  <div class="">
+    <a class="font-bold" href="/login">Click Here</a>
+  </div>
+</div>
