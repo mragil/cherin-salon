31220209c240ddf1fe2e7710267c3e4cd57a9fe0
Muhammad Ragil <mragiltrirezar@gmail.com>
2024-12-06 21:25:40 +0700

    feat: add search function

--- src/routes/pos/(components)/ItemList.svelte
+++ src/routes/pos/(components)/ItemList.svelte
@@ -1,13 +1,38 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input/index.js';
 	import type { ItemListProps } from '$lib/types';
 	import { formatRupiah } from '$lib/utils';
 
 	let { items, onPress }: ItemListProps = $props();
+	let searchTerm = $state('');
+	let filteredItems = $state(items);
+	$inspect(filteredItems);
+	$effect(() => {
+		if (searchTerm !== '') {
+			filteredItems = items.filter((item) =>
+				item.name.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+			return;
+		}
+		filteredItems = items;
+	});
 </script>
 
+<div class="mb-5">
+	<input
+		type="text"
+		class="w-2/5 p-3 border-primary focus:outline-none"
+		placeholder="Search by name"
+		bind:value={searchTerm}
+	/>
+</div>
+
 <div class="flex flex-wrap items-center justify-center gap-4">
-	{#each items as item}
+	{#if filteredItems.length === 0}
+		<p class="mt-20 text-2xl font-bold">{searchTerm} Not Found</p>
+	{/if}
+	{#each filteredItems as item}
 		<Card.Root
 			class="pressed max-w-64 flex-1 text-center"
 			onclick={() => {
