4020256deb54d8e58c5b3852aac0e17438f13280
Muhammad Ragil <mragiltrirezar@gmail.com>
2024-12-06 16:50:35 +0700

    feat: add ui filter date

---
+++ src/routes/admin/orders/(components)/DateFilter.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+	import { Calendar } from '$lib/components/ui/calendar/index.js';
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import { cn } from '$lib/utils.js';
+	import { DateFormatter, getLocalTimeZone, type DateValue } from '@internationalized/date';
+	import CalendarIcon from 'lucide-svelte/icons/calendar';
+
+	const df = new DateFormatter('id-ID', {
+		dateStyle: 'long'
+	});
+	let selectedDate = $state<DateValue | undefined>();
+	let contentRef = $state<HTMLElement | null>(null);
+	let {
+		onSelectedDate,
+		isOpenFilter
+	}: { onSelectedDate: (selectedDate: DateValue) => void; isOpenFilter: boolean } = $props();
+</script>
+
+<Popover.Root bind:open={isOpenFilter}>
+	<Popover.Trigger
+		class={cn(
+			buttonVariants({
+				variant: 'outline',
+				class: 'w-[280px] justify-start text-left font-normal'
+			}),
+			!selectedDate && 'text-muted-foreground'
+		)}
+	>
+		<CalendarIcon />
+		{selectedDate ? df.format(selectedDate.toDate(getLocalTimeZone())) : 'Pick a date'}
+	</Popover.Trigger>
+	<Popover.Content bind:ref={contentRef} class="flex w-auto flex-col items-center p-0">
+		<Calendar type="single" bind:value={selectedDate} />
+		<Button
+			class="w-full"
+			disabled={!selectedDate}
+			onclick={() => {
+				isOpenFilter = false;
+				onSelectedDate(selectedDate!);
+			}}>Filter</Button
+		>
+	</Popover.Content>
+</Popover.Root>
--- src/routes/admin/orders/+page.svelte
+++ src/routes/admin/orders/+page.svelte
@@ -1,15 +1,23 @@
-<script>
+<script lang="ts">
 	import { Skeleton } from '$lib/components/ui/skeleton';
+	import type { DateValue } from '@internationalized/date';
+	import DateFilter from './(components)/DateFilter.svelte';
 	import OrderTable from './(components)/OrderTable.svelte';
 
 	let { data } = $props();
+	let isOpenFilter = $state(false);
+
+	const onSelectedDate = (selectedDate: DateValue) => {
+		console.log(selectedDate);
+	};
 </script>
 
 <h1 class="text-3xl font-bold">Orders</h1>
 {#await data.orders}
 	<div class="flex gap-4">
-		<Skeleton class="h-full w-full" />
+		<Skeleton class="h-full w-full bg-gray-700" />
 	</div>
 {:then orders}
+	<DateFilter {onSelectedDate} {isOpenFilter}/>
 	<OrderTable {orders} />
 {/await}
