219a8b3ca7dfa714aa7da68d7fffca883c96ac55
Muhammad Ragil <mragiltrirezar@gmail.com>
2025-05-03 23:36:36 +0700

    feat: add alert about login info

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,6 +3,7 @@
 	import { pb } from '$lib/pocketbase';
 	import { toast } from 'svelte-sonner';
 
+	import * as Alert from '$lib/components/ui/alert/index.js';
 	import UserAuthForm from './(components)/user-auth-from.svelte';
 
 	const login = async (identity: string, password: string) => (callback: VoidFunction) => {
@@ -29,11 +30,21 @@
 <div class="flex h-screen items-center justify-center">
 	<div class="w-3/6">
 		<div class="mb-5 flex flex-col space-y-2 text-center">
-			<h1 class="text-2xl font-semibold tracking-tight">Cherin Salon</h1>
+			<h1 class="text-2xl font-semibold tracking-tight">Demo Shop</h1>
 			<p class="text-sm text-muted-foreground">
 				Enter your email or username and password to login
 			</p>
 		</div>
 		<UserAuthForm loginAction={login} />
+		<div class="mt-5">
+			<Alert.Root>
+				<Alert.Title>Heads up!</Alert.Title>
+				<Alert.Description>
+					You can use these credential to checkout the apps.
+					<br />Email: demo_admin@mrtrr.id - Password: demo_admin --> for admin access
+					<br />Email: demo_cashier@mrtrr.id - Password: demo_cashier --> for cashier access
+				</Alert.Description>
+			</Alert.Root>
+		</div>
 	</div>
 </div>
